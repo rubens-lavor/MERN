5371a9f443bfdbb5c8f60d11af655ef019ca1d39
rubens-lavor <haryson07@gmail.com>
2020-08-19 15:45:09 -0300

    mongoose. mondoDB compass

--- server.js
+++ server.js
@@ -2,14 +2,24 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const path = require("path");
+const mongoose = require("mongoose");
 
 const app = express();
-app.use(cors());
-
 const PORT = process.env.PORT || 5000;
 
-app.use(cookieParser());
+mongoose.connect("mongodb://localhost:27017/api-mern", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+}, function (err) {
+    if(err){
+        console.log(err);
+    }else {
+        console.log("Mongo CONECTADO com sucesso!");
+    }
+})
 
+app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
 
 app.get("/", function(req, res){
