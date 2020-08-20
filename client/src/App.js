986b8706605b13440c52037d6fca23b53ee7ad00
rubens-lavor <haryson07@gmail.com>
2020-08-20 17:19:23 -0300

    install material ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
+import Inicio from './pages/client/painel';
+
 
 function App() {
   return (
     <div className="App">
+      {/*
       <header className="App-header">
         <h1>oi mundo</h1>
       </header>
+      */}
+
+      <Inicio />
     </div>
   );
 }
