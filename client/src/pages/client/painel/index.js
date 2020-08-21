b4829e2ce4200270f2ed73464b1b1b4ce6a47af4
rubens-lavor <haryson07@gmail.com>
2020-08-20 22:52:28 -0300

    menu admin

--- client/src/pages/client/painel/index.js
+++ client/src/pages/client/painel/index.js
@@ -14,6 +14,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
 
+
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
