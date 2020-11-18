9eb9be86f899ed4b34de9b21c9334a955142443c
rubens-lavor <haryson07@gmail.com>
2020-11-17 22:46:35 -0300

    metodo isCorrectPassword

--- backend/src/models/usuarioModel.js
+++ backend/src/models/usuarioModel.js
@@ -29,5 +29,15 @@ DataSchema.pre("findOneAndUpdate", function(next){
   next();
 });
 
+DataSchema.method.isCorrectPassword = function (password, callback){
+    bcrypt.compare(password, this.senha_usuario,function(err,same){
+        if (err){
+            callback(err)
+        }else {
+            callback(err, same)
+        }
+    })
+}
+
 const usuarios = mongoose.model("Usuario", DataSchema);
 module.exports = usuarios;
