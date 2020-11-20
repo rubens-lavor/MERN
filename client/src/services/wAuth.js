e31ad5fd1ee4ab16cf825d04eb6c2503f53d0216
rubens-lavor <haryson07@gmail.com>
2020-11-19 21:50:32 -0300

    update

--- client/src/services/wAuth.js
+++ client/src/services/wAuth.js
@@ -3,15 +3,32 @@ import api from './api'
 import { login, logout, getToken } from './auth'
 import { Route, Redirect } from 'react-router-dom'
 
-export default function WAuth({ component:Component, ...rest }){
-    const[ redirect, setRedirect ] = useState(false)
-    const [ loading, setLoading ] = useState(true)
+export default function WAuth({ component: Component, ...rest }) {
+    const [redirect, setRedirect] = useState(false)
+    const [loading, setLoading] = useState(true)
 
 
-    useEffect(()=>{
-        async function verify(){
-            var res = await api.get('/gettoken',{params:{token:getToken()}})
+    useEffect(() => {
+        async function verify() {
+            var res = await api.get('/gettoken', { params: { token: getToken() } })
+
+            if (res.data.status === 200) {
+                setLoading(false)
+                setRedirect(false)
+            } else {
+                logout()
+                setLoading(false)
+                setRedirect(true)
+            }
         }
         verify()
-    })
+    }, [])
+
+    return (
+        loading ? 'Carregando...' : <Route {...rest}
+            render={props => !redirect ?
+                <Component {...props} /> :
+                <Redirect to={{ pathname: '/admin/login', state: { from: props.location } }} />
+            } />
+    )
 }
