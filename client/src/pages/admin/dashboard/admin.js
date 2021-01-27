ecd16b1f826164276f8ff07742b32affcc3c63e2
rubens-lavor <haryson07@gmail.com>
2021-01-26 23:02:22 -0300

    pag. admin

---
+++ client/src/pages/admin/dashboard/admin.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ImgAdmin from "../../../assets/img/painel-mock.png"
+
+export default function Dashboard() {
+
+    //const classes = useStyles();
+
+    return (
+        <img src={ImgAdmin} />
+    );
+}
