2fcd5187cee27b749eab13ba1ead7083cac533b0
rubens-lavor <haryson07@gmail.com>
2021-01-26 23:10:56 -0300

    update

--- backend/src/controllers/usuariosControllers.js
+++ backend/src/controllers/usuariosControllers.js
@@ -49,7 +49,7 @@ module.exports = {
     },
     async login(req, res) {
         const { email, senha } = req.body;
-        Usuario.findOne({ email_usuario: email, tipo_usuario: 1 }, function (err, user) {
+        Usuario.findOne({ email_usuario: email /*, tipo_usuario: 1*/ }, function (err, user) {
             if (err) {
                 console.log(err);
                 res.status(200).json({ erro: "Erro no servidor. Por favor, tente novamente" })
