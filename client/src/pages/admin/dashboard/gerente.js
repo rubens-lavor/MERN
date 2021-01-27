0a91e9a18bcbc39a8c9ae9ca5523407abd15856c
rubens-lavor <haryson07@gmail.com>
2021-01-26 23:02:11 -0300

    pag. gerente

---
+++ client/src/pages/admin/dashboard/gerente.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ImgAdmin from "../../../assets/img/gerente.png"
+
+export default function Dashboard() {
+
+    //const classes = useStyles();
+
+    return (
+        <img src={ImgAdmin} />
+    );
+}
