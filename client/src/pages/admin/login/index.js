b8209fcc1314cd443e0f7a72626f5160b1eb6b39
rubens-lavor <haryson07@gmail.com>
2021-02-02 22:26:33 -0300

    update

--- client/src/pages/admin/login/index.js
+++ client/src/pages/admin/login/index.js
@@ -12,7 +12,7 @@ import Container from '@material-ui/core/Container';
 
 import api from '../../../services/api'
 
-import {setNomeUsuario, login, setIdUsuario, setTipoUsuario} from '../../../services/auth'
+import { setNomeUsuario, login, setIdUsuario, setTipoUsuario } from '../../../services/auth'
 
 
 import IconButton from '@material-ui/core/IconButton';
@@ -30,93 +30,97 @@ import VisibilityOff from '@material-ui/icons/VisibilityOff';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
 function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="#">
-        Curso MERN
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="#">
+                Curso MERN
       </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
 }
 
 const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
 }));
 
 export default function SignIn() {
-  const classes = useStyles();
-  const [ email, setEmail ] = useState('')
-  const [ senha, setSenha ] = useState('')
-  const [ showPassword, setShowPassword ] = useState(false)
-  const [ loading, setLoading ] = useState(false)
+    const classes = useStyles();
+    const [email, setEmail] = useState('')
+    const [senha, setSenha] = useState('')
+    const [showPassword, setShowPassword] = useState(false)
+    const [loading, setLoading] = useState(false)
 
-  async function handleSubmit(){
-      setLoading(true)
-    await api.post('/api/usuarios/login',{email,senha})
-    .then(res => {
-        if(res.status === 200){
-            if(res.data.status === 1){
-                login(res.data.token)
-                setIdUsuario(res.data.id_client)
-                setNomeUsuario(res.data.user_name)
-                setTipoUsuario(res.data.user_type)
+    async function handleSubmit() {
+        await api.post('/api/usuarios/login', { email, senha })
+            .then(res => {
+                if (res.status === 200) {
+                    if (res.data.status === 1) {
+                        login(res.data.token)
+                        setIdUsuario(res.data.id_client)
+                        setNomeUsuario(res.data.user_name)
+                        setTipoUsuario(res.data.user_type)
 
-                window.location.href = '/admin'
-            }else if (res.data.status === 2){
-                alert('Atenção: ' + res.data.error)
-            }
-            setLoading(false)
-        }else{
-            alert('Erro no seridor')
-            setLoading(false)
-        }
-    })
-  }
+                        window.location.href = '/admin'
+                    } else if (res.data.status === 2) {
+                        alert('Atenção: ' + res.data.error)
+                    }
+                    setLoading(false)
+                } else {
+                    alert('Erro no seridor')
+                    setLoading(false)
+                }
+            })
+    }
 
-  return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Login
+    function loadingSubmit() {
+        setLoading(true)
+        setTimeout(()=>handleSubmit(),2000)
+    }
+
+    return (
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Login
         </Typography>
 
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Digite seu email"
-            name="email"
-            autoComplete="email"
-            autoFocus
-            value={email}
-            onChange={e=>setEmail(e.target.value)}
-          />
-          {/*}
+                <TextField
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="email"
+                    label="Digite seu email"
+                    name="email"
+                    autoComplete="email"
+                    autoFocus
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                />
+                {/*}
           <TextField
             variant="outlined"
             margin="normal"
@@ -131,43 +135,43 @@ export default function SignIn() {
             onChange={e=>setSenha(e.target.value)}
           />
         {*/}
-          <FormControl variant="outlined" style={{width:'100%', marginTop:10}}>
-          <InputLabel htmlFor="campoSenha">Digite sua senha</InputLabel>
-          <OutlinedInput
-            id="campoSenha"
-            type={showPassword ? 'text' : 'password'}
-            value={senha}
-            onChange={handleChange('password')}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={ e => setShowPassword(!showPassword)}
-                  edge="end"
-                >
-                  {showPassword ? <Visibility /> : <VisibilityOff />}
-                </IconButton>
-              </InputAdornment>
-            }
-            labelWidth={120}
-          />
-          </FormControl>
-          
-          <Button
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-            onClick={handleSubmit}
-            disabled={loading}
-          >
-              {loading? <CircularProgress />:"ENTRAR"} 
-          </Button>
-      </div>
+                <FormControl variant="outlined" style={{ width: '100%', marginTop: 10 }}>
+                    <InputLabel htmlFor="campoSenha">Digite sua senha</InputLabel>
+                    <OutlinedInput
+                        id="campoSenha"
+                        type={showPassword ? 'text' : 'password'}
+                        value={senha}
+                        onChange={handleChange('password')}
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={e => setShowPassword(!showPassword)}
+                                    edge="end"
+                                >
+                                    {showPassword ? <Visibility /> : <VisibilityOff />}
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                        labelWidth={120}
+                    />
+                </FormControl>
 
-      <Box mt={8}>
-        <Copyright />
-      </Box>
-    </Container>
-  );
+                <Button
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    className={classes.submit}
+                    onClick={loadingSubmit}
+                    disabled={loading}
+                >
+                    {loading ? <CircularProgress /> : "ENTRAR"}
+                </Button>
+            </div>
+
+            <Box mt={8}>
+                <Copyright />
+            </Box>
+        </Container>
+    );
 }
