622a70d3630ca0819042e2436101bdb4d44dea67
rubens-lavor <haryson07@gmail.com>
2021-02-03 21:28:17 -0300

    barra de pprogresso

--- client/src/services/wAuth.js
+++ client/src/services/wAuth.js
@@ -4,6 +4,8 @@ import api from './api';
 import { login, logout, getToken } from './auth';
 import { Route, Redirect } from 'react-router-dom';
 
+import LinearProgress from '@material-ui/core/LinearProgress';
+
 export default function WAuth ({ component: Component, ...rest }){
     const [ redirect, setRedirect ] = useState(false);
     const [ loading, setLoading ] = useState(true);
@@ -22,12 +24,13 @@ export default function WAuth ({ component: Component, ...rest }){
                 setRedirect(true);
             }
         }
-        verify();
+        setTimeout(()=>verify(),1500)
+        //verify();
     },[])
 
 
     return(
-        loading?'Carregando...':<Route { ...rest}
+        loading?<LinearProgress />:<Route { ...rest}
         render={props => !redirect?(
             <Component {...props } />
         ):<Redirect to={{pathname: "/admin/login",state:{ from: props.location}}} />
